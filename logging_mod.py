0c72e55f6f3fffc4b83f8f6c53f21d8cee7d5a02
AlexV174 <viny_174@mail.ru>
2022-01-30 17:30:03 +0500

    Lesson_3,4

---
+++ logging_mod.py
@@ -0,0 +1,22 @@
+from reusepatterns.singletones import SingletonByName
+from time import time
+
+
+class Logger(metaclass=SingletonByName):
+
+    def __init__(self, name):
+        self.name = name
+
+    def log(self, text):
+        print('log--->', text)
+
+
+def debug(func):
+    def inner(*args, **kwargs):
+        start = time()
+        result = func(*args, **kwargs)
+        end = time()
+        print('DEBUG-------->', func.__name__, end - start)
+        return result
+
+    return inner
